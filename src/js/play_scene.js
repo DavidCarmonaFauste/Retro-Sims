37457ac573ef001ab434079353d91af635408958
mariotab28 <38128815+mariotab28@users.noreply.github.com>
2018-10-24 13:38:26 +0200

    Revert "Añadidos archivos para las clases (aún no funciona)"
    This reverts commit efa51a88544a70be71a99246ec4eb21425837bdb.

--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -1,44 +1,39 @@
 'use strict';
 
-//requires
-//var Neighbour = require('./Neighbour');
-
-
-//Función principal
-var PlayScene = {
+  var PlayScene = {
   create: function () {
     //var logo = this.game.add.sprite(
-    //this.game.world.centerX, this.game.world.centerY, 'logo');
+      //this.game.world.centerX, this.game.world.centerY, 'logo');
     //logo.anchor.setTo(0.5, 0.5);
 
     var vecino = new Neighbour(0, 'logo', this.game);
+
     console.log(vecino.x + ' ' + vecino.y);
   }
 };
 
+function Entity ( dimX,  dimY, sprite, game){
+this.dimX = dimX;
+this.dimY = dimY;
 
-function Entity(dimX, dimY, sprite, game) {
-  this.dimX = dimX;
-  this.dimY = dimY;
-
-  Phaser.Sprite.call(this, game, game.world.centerX, game.world.centerY, sprite);
+Phaser.Sprite.call(this, game, game.world.centerX, game.world.centerY, sprite);
 }
 
 Entity.prototype = Object.create(Phaser.Sprite.prototype);
 Entity.prototype.constructor = Entity;
 
-function Neighbour(friendship, sprite, game) {
+function Neighbour(friendship, sprite, game){
   this.friendship = friendship;
-  Entity.call(this, 1, 1, sprite, game);
+  Entity.call(this,1,1, sprite, game);
 }
 
 Neighbour.prototype = Object.create(Entity.prototype);
 Neighbour.prototype.constructor = Neighbour;
 
 function Needs(pee, hunger, fatigue) {
-  this.pee = pee;
-  this.hunger = hunger;
-  this.fatigue = fatigue;
+this.pee = pee;
+this.hunger = hunger;
+this.fatigue = fatigue;
 }
 
 function Stats(intelligence, fitness, charisma) {
@@ -47,23 +42,23 @@ function Stats(intelligence, fitness, charisma) {
   this.charisma = charisma;
 }
 
-function Player(pee, hunger, fatigue, intelligence, fitness, charisma, money, job, dimX, dimY, sprite, game) {
+function Player (pee, hunger, fatigue, intelligence, fitness, charisma, money, job, dimX,  dimY, sprite, game){
   this.needs = new Needs(pee, hunger, fatigue);
   this.stats = new Stats(intelligence, fitness, charisma);
   this.money = money;
   this.job = job;
 
-  Entity.call(this, 1, 1, sprite, game);
+  Entity.call(this,1,1, sprite, game);
 }
 
 Player.prototype = Object.create(Entity.prototype);
 Player.prototype.constructor = Player;
 
-function Furniture(interactive, type, name, dimX, dimY, sprite, game) {
+function Furniture(interactive, type, name, dimX,  dimY, sprite, game){
   this.interactive = interactive;
   this.type = type;
   this.name = name;
-
+  
   Entity.call(this, dimX, dimY, sprite, game);
 }
 
