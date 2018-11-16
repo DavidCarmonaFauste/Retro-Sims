853335b0c6072aa77dd396ed778b06e7c7957ad6
DavidCarmonaFauste <davicarm@ucm.es>
2018-11-16 17:25:15 +0100

    ya no peta

--- src/js/creation_scene.js
+++ src/js/creation_scene.js
@@ -3,12 +3,12 @@
 var CreationScene = {
     create: function () {
         console.log('You are in creation scene');
-        for (int i = 1; i < 11; i++) {
+        for (var i = 1; i < 11; i++) {
             var character = this.game.add.sprite(
             this.game.world.centerX, this.game.world.centerY, 'Sim'+i);
             }
-        logo.anchor.setTo(0.5, 0.5);
-        logo.scale.setTo(0.75, 0.75);
+        character.anchor.setTo(0.5, 0.5);
+        character.scale.setTo(0.75, 0.75);
 
     },
 
